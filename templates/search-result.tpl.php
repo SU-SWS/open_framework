c2271127a8163a9dbc62f6fd6c67dd944c31bc31
Brian Young <brianyoung@stanford.edu>
2012-03-23 11:27:19 -0700

    Removed author information from search results Fixed #1

---
+++ templates/search-result.tpl.php
@@ -0,0 +1,8 @@
+<dt class="title">
+  <a href="<?php print $url; ?>"><?php print $title; ?></a>
+</dt>
+<dd>
+  <?php if ($snippet) : ?>
+    <p class="search-snippet"><?php print $snippet; ?></p>
+  <?php endif; ?>
+</dd>
