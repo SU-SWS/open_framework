ef4f1817b199b643ac0356a7996170fcfa529ea0
Brian Young <brianyoung@stanford.edu>
2012-08-30 07:58:29 -0700

    Updated block regions

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -10,7 +10,6 @@
   </div>
 </div>
 <!-- /#skipnav -->
-
 <div id="header" class="clearfix">
   <div class="container">
     <?php if ($logo): ?>
@@ -35,7 +34,6 @@
   </div>
 </div>
 <!-- /#header -->
-
 <?php if ($main_menu): ?>
 <div id="main-menu" class="clearfix">
   <div class="container"> <?php print render($main_menu_expanded); ?> </div>
@@ -48,52 +46,73 @@
 </div>
 <!-- /#messages -->
 <?php endif; ?>
-<?php if ($page['featured']): ?>
-<div id="featured" class="clearfix">
-  <div class="container"> <?php print render($page['featured']); ?> </div>
-</div>
-<!-- /#featured -->
-<?php endif; ?>
 <div id="main" class="clearfix">
   <div class="container">
     <?php if ($breadcrumb): ?>
     <div id="breadcrumb"><?php print $breadcrumb; ?></div>
     <?php endif; ?>
+    <?php if ($page['page_top']): ?>
+    <div id="page-top" class="row"> <?php print render($page['page_top']); ?> </div>
+    <?php endif; ?>
+    <?php if ($page['page_upper']): ?>
+    <div id="page-upper" class="row"> <?php print render($page['page_upper']); ?> </div>
+    <?php endif; ?>
     <div class="row">
       <?php if ($page['sidebar_first']): ?>
-      <div id="sidebar-first" class="sidebar span3"> <?php print render($page['sidebar_first']); ?> </div>
+      <div id="sidebar-first" class="sidebar span3">
+        <div class="row-fluid"><?php print render($page['sidebar_first']); ?></div>
+      </div>
       <!-- /#sidebar-first -->
       <?php endif; ?>
       <div id="content" class="<?php if (($page['sidebar_first']) && ($page['sidebar_second'])): print 'span6'; elseif (($page['sidebar_first']) || ($page['sidebar_second'])): print 'span9'; else: print 'span12'; endif; ?>">
         <?php if ($page['highlighted']): ?>
-        <div id="highlighted"><?php print render($page['highlighted']); ?></div>
+        <div id="highlighted" class="row-fluid"><?php print render($page['highlighted']); ?></div>
         <?php endif; ?>
-        <a id="main-content"></a> <?php print render($title_prefix); ?>
-        <?php if ($title): ?>
-        <h1 class="title" id="page-title"> <?php print $title; ?> </h1>
+        <?php if ($page['content_top']): ?>
+        <div id="content-top" class="row-fluid"> <?php print render($page['content_top']); ?> </div>
         <?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php if ($tabs): ?>
-        <div class="tabs"> <?php print render($tabs); ?> </div>
+        <?php if ($page['content_upper']): ?>
+        <div id="content-upper" class="row-fluid"> <?php print render($page['content_upper']); ?> </div>
         <?php endif; ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?>
-        <ul class="action-links">
-          <?php print render($action_links); ?>
-        </ul>
+        <div id="main-content" class="row-fluid"> <?php print render($title_prefix); ?>
+          <?php if ($title): ?>
+          <h1 class="title" id="page-title"> <?php print $title; ?> </h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php if ($tabs): ?>
+          <div class="tabs"> <?php print render($tabs); ?> </div>
+          <?php endif; ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?>
+          <ul class="action-links">
+            <?php print render($action_links); ?>
+          </ul>
+          <?php endif; ?>
+          <?php print render($page['content']); ?> <?php print $feed_icons; ?> </div>
+        <?php if ($page['content_lower']): ?>
+        <div id="content-lower" class="row-fluid"> <?php print render($page['content_lower']); ?> </div>
         <?php endif; ?>
-        <?php print render($page['content']); ?> <?php print $feed_icons; ?> </div>
+        <?php if ($page['content_bottom']): ?>
+        <div id="content-bottom" class="row-fluid"> <?php print render($page['content_bottom']); ?> </div>
+        <?php endif; ?>
+      </div>
       <!-- /#content -->
-      
       <?php if ($page['sidebar_second']): ?>
-      <div id="sidebar-second" class="sidebar span3"> <?php print render($page['sidebar_second']); ?> </div>
+      <div id="sidebar-second" class="sidebar span3">
+        <div class="row-fluid"><?php print render($page['sidebar_second']); ?></div>
+      </div>
       <!-- /#sidebar-second -->
       <?php endif; ?>
     </div>
+    <?php if ($page['page_lower']): ?>
+    <div id="page-lower" class="row"> <?php print render($page['page_lower']); ?> </div>
+    <?php endif; ?>
+    <?php if ($page['page_bottom']): ?>
+    <div id="page-bottom" class="row"> <?php print render($page['page_bottom']); ?> </div>
+    <?php endif; ?>
   </div>
 </div>
 <!-- /#main, /#main-wrapper -->
-
 <?php if ($secondary_menu): ?>
 <div id="secondary-menu" class="navigation">
   <div class="container"> <?php print theme('links__system_secondary_menu', array(
@@ -119,4 +138,4 @@
     <?php endif; ?>
   </div>
 </div>
-<!-- /.section, /#footer-wrapper --> 
+<!-- /.section, /#footer-wrapper -->
