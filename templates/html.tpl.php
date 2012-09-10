7dfafb4df91eb47fd0c2dfc4eef196f7143c9e2b
Brian Young <brianyoung@stanford.edu>
2012-09-10 16:27:34 -0700

    Removed IE content flash javascript

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -4,9 +4,9 @@
 <title><?php print $head_title; ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php print $head; ?><?php print $styles; ?><?php print $scripts; ?>
-<script type="text/javascript"><?php /* Needed to avoid Flash of Unstyled Content in IE */ ?> </script>
+<!-- IE Fix for HTML5 Tags -->
 <!--[if lt IE 9]>
-<script src="https://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
 </head>
 <body class="<?php print $classes; ?> <?php print $attributes;?>">
