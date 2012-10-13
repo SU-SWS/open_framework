992a7acd285eee0ab8d6f470f44adb80824d908d
Brian Young <brianyoung@stanford.edu>
2012-10-12 18:24:44 -0700

    Updated block responsive behavior

--- js/open_framework.js
+++ js/open_framework.js
@@ -21,15 +21,16 @@ $(document).ready(function(){
 		$('.two-sidebars #content').removeClass('span6');
 		$('.two-sidebars #content').addClass('span8');
 		$('.two-sidebars .region-sidebar-second .block').addClass('span4');
-
-    	$('.one-sidebar #sidebar-first').removeClass('span3');
-		$('.one-sidebar #sidebar-first').addClass('span4');
-		$('.one-sidebar #sidebar-second').removeClass('span3');
-		$('.one-sidebar #sidebar-second').addClass('span4');
-		$('.one-sidebar #content').removeClass('span9');
-		$('.one-sidebar #content').addClass('span8');
+    	$('.sidebar-first #sidebar-first').removeClass('span3');
+		$('.sidebar-first #sidebar-first').addClass('span4');
+		$('.sidebar-first #content').removeClass('span9');
+		$('.sidebar-first #content').addClass('span8');
+		$('.sidebar-second #sidebar-second').removeClass('span3');
+		$('.sidebar-second #sidebar-second').addClass('span12');
+		$('.sidebar-first #content').removeClass('span9');
+		$('.sidebar-first #content').addClass('span12');
 		}
-		
+
 		else {
 		$('.two-sidebars #sidebar-first').removeClass('span4');
 		$('.two-sidebars #sidebar-first').addClass('span3');
@@ -38,13 +39,14 @@ $(document).ready(function(){
 		$('.two-sidebars #content').removeClass('span8');
 		$('.two-sidebars #content').addClass('span6');
 		$('.two-sidebars .region-sidebar-second .block').removeClass('span4');
-
-    	$('.one-sidebar #sidebar-first').removeClass('span4');
-		$('.one-sidebar #sidebar-first').addClass('span3');
-		$('.one-sidebar #sidebar-second').removeClass('span4');
-		$('.one-sidebar #sidebar-second').addClass('span3');
-		$('.one-sidebar #content').removeClass('span8');
-		$('.one-sidebar #content').addClass('span9');
+    	$('.sidebar-first #sidebar-first').removeClass('span4');
+		$('.sidebar-first #sidebar-first').addClass('span3');
+		$('.sidebar-first #content').removeClass('span8');
+		$('.sidebar-first #content').addClass('span9');
+		$('.sidebar-second #sidebar-second').removeClass('span12');
+		$('.sidebar-second #sidebar-second').addClass('span3');
+		$('.sidebar-first #content').removeClass('span12');
+		$('.sidebar-first #content').addClass('span9');
 		}
 	});
 	
@@ -60,12 +62,14 @@ $(document).ready(function(){
 		$('.two-sidebars #content').removeClass('span6');
 		$('.two-sidebars #content').addClass('span8');
 		$('.two-sidebars .region-sidebar-second .block').addClass('span4');
-    	$('.one-sidebar #sidebar-first').removeClass('span3');
-		$('.one-sidebar #sidebar-first').addClass('span4');
-		$('.one-sidebar #sidebar-second').removeClass('span3');
-		$('.one-sidebar #sidebar-second').addClass('span4');
-		$('.one-sidebar #content').removeClass('span9');
-		$('.one-sidebar #content').addClass('span8');
+    	$('.sidebar-first #sidebar-first').removeClass('span3');
+		$('.sidebar-first #sidebar-first').addClass('span4');
+		$('.sidebar-first #content').removeClass('span9');
+		$('.sidebar-first #content').addClass('span8');
+		$('.sidebar-second #sidebar-second').removeClass('span3');
+		$('.sidebar-second #sidebar-second').addClass('span12');
+		$('.sidebar-first #content').removeClass('span9');
+		$('.sidebar-first #content').addClass('span12');
 		}
 
 		else {
@@ -76,12 +80,14 @@ $(document).ready(function(){
 		$('.two-sidebars #content').removeClass('span8');
 		$('.two-sidebars #content').addClass('span6');
 		$('.two-sidebars .region-sidebar-second .block').removeClass('span4');
-    	$('.one-sidebar #sidebar-first').removeClass('span4');
-		$('.one-sidebar #sidebar-first').addClass('span3');
-		$('.one-sidebar #sidebar-second').removeClass('span4');
-		$('.one-sidebar #sidebar-second').addClass('span3');
-		$('.one-sidebar #content').removeClass('span8');
-		$('.one-sidebar #content').addClass('span9');
+    	$('.sidebar-first #sidebar-first').removeClass('span4');
+		$('.sidebar-first #sidebar-first').addClass('span3');
+		$('.sidebar-first #content').removeClass('span8');
+		$('.sidebar-first #content').addClass('span9');
+		$('.sidebar-second #sidebar-second').removeClass('span12');
+		$('.sidebar-second #sidebar-second').addClass('span3');
+		$('.sidebar-first #content').removeClass('span12');
+		$('.sidebar-first #content').addClass('span9');
 		}
 	});
 });
