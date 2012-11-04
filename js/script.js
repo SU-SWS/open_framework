94eb5faabfcecf970099c219616f71fdeaea6d25
Brian Young <brianyoung@stanford.edu>
2012-11-03 19:37:52 -0700

    Updated main menu navigation hover style

--- js/script.js
+++ js/script.js
@@ -6,10 +6,13 @@ $(document).ready(function(){
 	$('#main-menu ul ul').removeClass('nav');
 	$('#main-menu ul li.expanded').addClass('dropdown');
 	$('#main-menu ul li.expanded a').addClass('dropdown-toggle');
+	// $('#main-menu ul li.expanded a').attr('data-toggle', 'dropdown');
 	$('#main-menu ul li.expanded ul').addClass('dropdown-menu');
 	$('#main-menu ul.dropdown-menu li').removeClass('active');
 	$('#main-menu ul li').removeClass('expanded collapsed leaf');
 	$('#main-menu ul li:has(.active)').addClass('active');
 	$('#main-menu ul.nav > li > a.dropdown-toggle').append(' <b class="caret"></b>');
 });
+
+
 
