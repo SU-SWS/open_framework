c851dc0ec099ac2a545b3ed07e3c421c3c1c1701
Brian Young <brianyoung@stanford.edu>
2013-01-28 14:04:02 -0800

    Revised and optimized responsive menu link overrides

--- js/override.js
+++ js/override.js
@@ -3,34 +3,30 @@
   Drupal.behaviors.open_framework = {
     attach: function (context, settings) {
 		
-			// Bootstrap Dropdown Menu
+			// Bootstrap dropdown menu in top nav bar
 			$('nav ul > li:has(.active)').addClass('active');
 			$('nav ul > li:has(ul .active)').removeClass('active');			
 	        $('nav ul > li > ul > li:has(.active)').removeClass('active');	
+			$('nav ul ul a').removeAttr('data-toggle','data-target');
 			
 			// Turn off Bootstrap dropdown data attributes in sidebars
-			$('#sidebar-first a').removeAttr('data-toggle');
-			$('#sidebar-first a').removeAttr('data-target');
-			$('#sidebar-first ul').removeClass('dropdown-menu');
-			$('#sidebar-first li').removeClass('dropdown-submenu');
-
-			$('#sidebar-second a').removeAttr('data-toggle');
-			$('#sidebar-second a').removeAttr('data-target');
-			$('#sidebar-second ul').removeClass('dropdown-menu');
-			$('#sidebar-second li').removeClass('dropdown-submenu');
+			$('#sidebar-first a').removeAttr('data-toggle','data-target');
+			$('#sidebar-first ul').removeClass('dropdown-menu','dropdown-submenu');
+			$('#sidebar-second a').removeAttr('data-toggle','data-target');
+			$('#sidebar-second ul').removeClass('dropdown-menu','dropdown-submenu');
 			
-			// Update CSS classes based on window load
-			$(window).load(function() {
+			// Set up theme specific responsive behaviors
+			function responsive_behaviors () {
 				var width = $(window).width();
 				
 				if (width < 751) {
-				$('nav li').removeClass('dropdown-submenu');
+				$('nav li li.expanded').removeClass('dropdown-submenu');
 				$('nav ul ul ul').removeClass('dropdown-menu');
 				}
 				
 				else {
 				$('nav li li.expanded').addClass('dropdown-submenu');
-				$('nav ul ul:has(ul)').addClass('dropdown-menu');
+				$('nav ul ul ul').addClass('dropdown-menu');
 				}
 				
 				if ((width >= 751) && (width < 963)) {
@@ -70,60 +66,11 @@
 				$('.sidebar-second #content').addClass('span9');
 				$('.sidebar-second .region-sidebar-second .block').removeClass('span4');
 				}
-			});
-
-			// Update CSS classes based on window resize
-			$(window).resize(function() {
-				var width = $(window).width();
-
-				if (width < 751) {
-				$('nav li').removeClass('dropdown-submenu');
-				$('nav ul ul ul').removeClass('dropdown-menu');
-				}
-				
-				else {
-				$('nav li li.expanded').addClass('dropdown-submenu');
-				$('nav ul ul:has(ul)').addClass('dropdown-menu');
-				}
-				
-				if ((width >= 751) && (width < 963)) {
-				$('.two-sidebars #sidebar-first').removeClass('span3');
-				$('.two-sidebars #sidebar-first').addClass('span4');
-				$('.two-sidebars #sidebar-second').removeClass('span3');
-				$('.two-sidebars #sidebar-second').addClass('span12');
-				$('.two-sidebars #content').removeClass('span6');
-				$('.two-sidebars #content').addClass('span8');
-				$('.two-sidebars .region-sidebar-second .block').addClass('span4');
-		    	$('.sidebar-first #sidebar-first').removeClass('span3');
-				$('.sidebar-first #sidebar-first').addClass('span4');
-				$('.sidebar-first #content').removeClass('span9');
-				$('.sidebar-first #content').addClass('span8');
-				$('.sidebar-second #sidebar-second').removeClass('span3');
-				$('.sidebar-second #sidebar-second').addClass('span12');
-				$('.sidebar-second #content').removeClass('span9');
-				$('.sidebar-second #content').addClass('span12');
-				$('.sidebar-second .region-sidebar-second .block').addClass('span4');
-				}
-
-				else {
-				$('.two-sidebars #sidebar-first').removeClass('span4');
-				$('.two-sidebars #sidebar-first').addClass('span3');
-				$('.two-sidebars #sidebar-second').removeClass('span12');
-				$('.two-sidebars #sidebar-second').addClass('span3');
-				$('.two-sidebars #content').removeClass('span8');
-				$('.two-sidebars #content').addClass('span6');
-				$('.two-sidebars .region-sidebar-second .block').removeClass('span4');
-		    	$('.sidebar-first #sidebar-first').removeClass('span4');
-				$('.sidebar-first #sidebar-first').addClass('span3');
-				$('.sidebar-first #content').removeClass('span8');
-				$('.sidebar-first #content').addClass('span9');
-				$('.sidebar-second #sidebar-second').removeClass('span12');
-				$('.sidebar-second #sidebar-second').addClass('span3');
-				$('.sidebar-second #content').removeClass('span12');
-				$('.sidebar-second #content').addClass('span9');
-				$('.sidebar-second .region-sidebar-second .block').removeClass('span4');
-				}
-			});
+			}
+			
+			// Update CSS classes based on window load and resize
+			$(window).load(responsive_behaviors);
+			$(window).resize(responsive_behaviors);
 
 		}
 	}
