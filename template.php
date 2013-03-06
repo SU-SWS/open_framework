57ef99da674304ddbacd1044ae473f64e7cc0030
Megan Miller <meganem@stanford.edu>
2013-03-06 12:34:11 -0800

    Limiting bootstrap drop-down styles to main menu only

--- template.php
+++ template.php
@@ -282,36 +282,49 @@ function open_framework_menu_tree(&$vars) {
 }
 
 function open_framework_menu_link(array $vars) {
-  $element = $vars['element'];
-  $sub_menu = '';
-  
-  if ($element['#below']) {
-    // Ad our own wrapper
-    unset($element['#below']['#theme_wrappers']);
-    $sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
-    $element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
-    $element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
-
-    // Check if this element is nested within another
-    if ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] > 1)) {
-      // Generate as dropdown submenu
-      $element['#attributes']['class'][] = 'dropdown-submenu';
-    }
-    else {
-      // Generate as standard dropdown
-      $element['#attributes']['class'][] = 'dropdown';
-      $element['#localized_options']['html'] = TRUE;
-      $element['#title'] .= ' <span class="caret"></span>';
-    }
-
-    // Set dropdown trigger element to # to prevent inadvertant page loading with submenu click
-    $element['#localized_options']['attributes']['data-target'] = '#';
-  }
-  
-  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
-  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+	// only apply bootstrap menu classes to main_menu (uses machine menu name)
+	if ($vars['element']['#theme'] == 'menu_link__main_menu') {
+	
+	  $element = $vars['element'];
+	  $sub_menu = '';
+	  
+	  if ($element['#below']) {
+		// Ad our own wrapper
+		unset($element['#below']['#theme_wrappers']);
+		$sub_menu = '<ul class="dropdown-menu">' . drupal_render($element['#below']) . '</ul>';
+		$element['#localized_options']['attributes']['class'][] = 'dropdown-toggle';
+		$element['#localized_options']['attributes']['data-toggle'] = 'dropdown';
+	
+		// Check if this element is nested within another
+		if ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] > 1)) {
+		  // Generate as dropdown submenu
+		  $element['#attributes']['class'][] = 'dropdown-submenu';
+		}
+		else {
+		  // Generate as standard dropdown
+		  $element['#attributes']['class'][] = 'dropdown';
+		  $element['#localized_options']['html'] = TRUE;
+		  $element['#title'] .= ' <span class="caret"></span>';
+		}
+	
+		// Set dropdown trigger element to # to prevent inadvertant page loading with submenu click
+		$element['#localized_options']['attributes']['data-target'] = '#';
+	  }
+	  
+	  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+	  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+	  
+	} else {
+	  $element = $vars['element'];
+	  $sub_menu = '';
+	
+	  if ($element['#below']) {
+		$sub_menu = drupal_render($element['#below']);
+	  }
+	  $output = l($element['#title'], $element['#href'], $element['#localized_options']);
+	  return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
+	}
 }
-
 /**
 * Get all primary tasks including subsets
 */
