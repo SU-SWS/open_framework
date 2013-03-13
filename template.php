a0a34aee3e40000410c1c84dbbf0b5a2f4e146d0
Megan Miller <meganem@stanford.edu>
2013-03-13 15:08:42 -0700

    Adding preprocess function to add search button icon and styles

--- template.php
+++ template.php
@@ -240,7 +240,9 @@ function open_framework_form_alter(&$form, &$form_state, $form_id) {
     $form['search_block_form']['#title_display'] = 'invisible';
     $form['search_block_form']['#attributes']['class'][] = 'input-medium search-query';
 	$form['search_block_form']['#attributes']['placeholder'] = t('Search this site...');
-    $form['actions']['submit']['#attributes']['class'][] = 'btn btn-search';
+    $form['actions']['submit']['#attributes']['class'][] = 'btn-search';
+	$form['actions']['submit']['#type'] = 'image_button';
+	$form['actions']['submit']['#src'] = drupal_get_path('theme', 'open_framework') . '/images/searchbutton.png';
   }
 }
 
