c8087378d3f3944156bbcae36ed1eebdc3abab5b
Brian Young <brianyoung@stanford.edu>
2013-03-25 10:55:48 -0700

    Added WAI-ARIA role for search block

--- template.php
+++ template.php
@@ -249,14 +249,20 @@ function open_framework_status_messages($variables) {
 }
 
 /* Search Form Block */
+function search_preprocess_block(&$variables) {
+  if ($variables['block']->module == 'search') {
+    $variables['attributes_array']['role'] = 'search';
+  }
+}
+
 function open_framework_form_alter(&$form, &$form_state, $form_id) {
   if ($form_id == 'search_block_form') {
     $form['search_block_form']['#title_display'] = 'invisible';
     $form['search_block_form']['#attributes']['class'][] = 'input-medium search-query';
-	$form['search_block_form']['#attributes']['placeholder'] = t('Search this site...');
+    $form['search_block_form']['#attributes']['placeholder'] = t('Search this site...');
     $form['actions']['submit']['#attributes']['class'][] = 'btn-search';
-	$form['actions']['submit']['#type'] = 'image_button';
-	$form['actions']['submit']['#src'] = drupal_get_path('theme', 'open_framework') . '/images/searchbutton.png';
+    $form['actions']['submit']['#type'] = 'image_button';
+    $form['actions']['submit']['#src'] = drupal_get_path('theme', 'open_framework') . '/images/searchbutton.png';
   }
 }
 
