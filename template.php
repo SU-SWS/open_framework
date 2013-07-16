c0fc0be174838c8245b74a36a047950215aa1879
John Bickar <jbickar@stanford.edu>
2013-07-16 14:35:13 -0700

    Fixes #115

--- template.php
+++ template.php
@@ -267,7 +267,8 @@ function open_framework_form_alter(&$form, &$form_state, $form_id) {
     $form['search_block_form']['#attributes']['class'][] = 'input-medium search-query';
     $form['search_block_form']['#attributes']['placeholder'] = t('Search this site...');
     $form['actions']['submit']['#attributes']['class'][] = 'btn-search';
-	$form['actions']['submit']['#attributes']['alt'] = t('Search button');
+    $form['actions']['submit']['#attributes']['alt'] = t('Search button');
+    unset($form['actions']['submit']['#value']);    
     $form['actions']['submit']['#type'] = 'image_button';
     $form['actions']['submit']['#src'] = drupal_get_path('theme', 'open_framework') . '/images/searchbutton.png';
   }
