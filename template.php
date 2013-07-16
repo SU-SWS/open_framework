b05971538ca24b31910e2f56f15d411c993af44d
Brian Young <brianyoung@stanford.edu>
2013-07-16 13:57:54 -0700

    Added alt attribute to search box form submit input element #115

--- template.php
+++ template.php
@@ -267,6 +267,7 @@ function open_framework_form_alter(&$form, &$form_state, $form_id) {
     $form['search_block_form']['#attributes']['class'][] = 'input-medium search-query';
     $form['search_block_form']['#attributes']['placeholder'] = t('Search this site...');
     $form['actions']['submit']['#attributes']['class'][] = 'btn-search';
+	$form['actions']['submit']['#attributes']['alt'] = t('Search button');
     $form['actions']['submit']['#type'] = 'image_button';
     $form['actions']['submit']['#src'] = drupal_get_path('theme', 'open_framework') . '/images/searchbutton.png';
   }
