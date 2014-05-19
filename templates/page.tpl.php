d7a08d87fb130cccaac56d21be26ff9b3d25974f
Brian Young <brianyoung@stanford.edu>
2014-05-19 13:24:49 -0700

    Updated table display style in header section

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -11,7 +11,7 @@
 <div id="header" class="clearfix header" role="banner">
   <div class="container">
     <div class="row">
-      <div class="<?php if ($page['search_box']): print 'span8'; else: print 'span12'; endif; ?>">
+      <div class="header-section <?php if ($page['search_box']): print 'span8'; else: print 'span12'; endif; ?>">
         <?php if ($logo): ?>
         <div id="logo" class="site-logo"> <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"> <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" role="presentation" /> </a></div>
         <?php endif; ?>
