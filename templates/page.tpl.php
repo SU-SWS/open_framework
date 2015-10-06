71add44f53b8176c121ac0b4b064781eeab13815
Megan Miller <meganem@stanford.edu>
2015-10-06 09:57:17 -0700

    adding second fullwidth region right above footer

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -84,9 +84,9 @@
 <?php endif; ?>
 
 
-<?php if ($page['fullwidth']): ?>
-  <div id="fullwidth" class="row-fluid fullwidth">
-    <div class="container"> <?php print render($page['fullwidth']); ?> </div>
+<?php if ($page['fullwidth_top']): ?>
+  <div id="fullwidth-top" class="row-fluid fullwidth">
+    <div class="container"> <?php print render($page['fullwidth_top']); ?> </div>
   </div>
 <?php endif; ?>
 
@@ -235,6 +235,13 @@
   </div>
 </div>
 <!-- /#main, /#main-wrapper -->
+
+<?php if ($page['fullwidth_bottom']): ?>
+  <div id="fullwidth-bottom" class="row-fluid fullwidth">
+    <div class="container"> <?php print render($page['fullwidth_bottom']); ?> </div>
+  </div>
+<?php endif; ?>
+
 <?php if ($page['footer']): ?>
 <div id="footer" class="clearfix site-footer" role="contentinfo">
   <div class="container">
