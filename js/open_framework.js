b3735a512b79433e31044f862886b03f536c0d0f
Marco Wise <marco.wise@stanford.edu>
2015-12-03 11:23:06 -0800

    indent fix

--- js/open_framework.js
+++ js/open_framework.js
@@ -49,33 +49,32 @@ Drupal.behaviors.open_framework = {
     // Equal Column Height on load and resize
     // Credit: http://codepen.io/micahgodbolt/pen/FgqLc
     equalHeight = function(container){
-    var currentTallest = 0,
-         currentRowStart = 0,
-         rowDivs = new Array(),
-         $el,
-         topPosition = 0;
-     $(container).each(function() {
+      var currentTallest = 0,
+          currentRowStart = 0,
+          rowDivs = new Array(),
+          $el,
+          topPosition = 0;
+      $(container).each(function() {
+        $el = $(this);
+        $($el).height('auto')
+        topPosition = $el.position().top;
 
-       $el = $(this);
-       $($el).height('auto')
-       topPosition = $el.position().top;
-
-       if (currentRowStart != topPosition) {
-         for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-           rowDivs[currentDiv].height(currentTallest);
-         }
-         rowDivs.length = 0; // empty the array
-         currentRowStart = topPosition;
-         currentTallest = $el.height();
-         rowDivs.push($el);
-       } else {
-         rowDivs.push($el);
-         currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
-      }
-       for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
-         rowDivs[currentDiv].height(currentTallest);
-       }
-     });
+        if (currentRowStart != topPosition) {
+          for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+          }
+          rowDivs.length = 0; // empty the array
+          currentRowStart = topPosition;
+          currentTallest = $el.height();
+          rowDivs.push($el);
+        } else {
+          rowDivs.push($el);
+          currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+        }
+        for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+          rowDivs[currentDiv].height(currentTallest);
+        }
+      });
     }
 
     $(window).load(function() {
